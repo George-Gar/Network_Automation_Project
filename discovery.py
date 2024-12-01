1ceefa8036915734e62b9876754e37a7bcc07aeb
George Garcia <garciag1646@gmail.com>
2024-12-01 13:29:13 -0500

    .git ignore

--- discovery.py
+++ discovery.py
@@ -1,8 +1,11 @@
 from netmiko import ConnectHandler
+from dotenv import load_dotenv
 import nornir
 import yaml
 import os
 
+# Load environment variables from the .env file
+load_dotenv()
 
 class Discovery:
 
@@ -10,15 +13,18 @@ class Discovery:
         self.starting_ip = input('Please enter the IP of the device you are starting discovery on (this IP will be excluded from inventory): ')
         self.network_access_ip = input('Please enter the IP of the connection prividing network access (this IP will be excluded from inventory): ')
         self.excluded_ips = [self.starting_ip, self.network_access_ip]
+        self.switch_user = os.environ.get('switch_user')
+        self.switch_pass = os.environ.get('switch_pass')
+        self.switch_enable = os.environ.get('switch_enable')
 
     
     def device(self) -> dict:
         device = {
             'device_type': 'cisco_ios',  # Specify device type
             'host': self.starting_ip,       # Device IP address
-            'username': os.environ.get('switch_user'),          # SSH username
-            'password': os.environ.get('switch_pass'),   # SSH password
-            'secret': os.environ.get('switch_enable'),   # Enable password (if required)
+            'username': self.switch_user,          # SSH username
+            'password': self.switch_pass,   # SSH password
+            'secret': self.switch_enable,   # Enable password (if required)
             'port': 22,                   # Default SSH port (optional)
             }
         return device
